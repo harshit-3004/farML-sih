2278eb0a41848eb3e74071246b6e6e50cd36c3bb
harshit-3004 <h.chaubisa30@gmail.com>
2022-04-07 13:19:27 +0530

    adding the login.js file

--- login.js
+++ login.js
@@ -1,3 +1,19 @@
 function login_validation(){
-    
+    var email = document.getElementById('form2Example17').value;
+    var password = document.getElementById("form2Example27").value;
+
+    if(email == ""){
+        document.getElementById('email').innerHTML = "Please enter the email field";
+        return false;
+    }
+
+    else if(password == ""){
+        document.getElementById('email').innerHTML = "";
+        document.getElementById('password').innerHTML = "Please enter the password field";
+    }
+
+    else{
+        document.getElementById('password').innerHTML = "";
+        document.getElementById('loginbutton').appendChild = "<div class='modal fade' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><h5 class='modal-title' id='exampleModalLabel'>Welcome back! Proceed with your crop.</h5><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></div><div class='modal-body'><button type='button' class='btn btn-primary' onclick='window.location.href='./index.html''>Registered Crop</button><br><button type='button' class='btn btn-primary' style='margin-top: 5px;' onclick='window.location.href='./crop registration.html''>New Crop</button></div></div></div></div>";
+    }
 }
